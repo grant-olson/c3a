4a5b6a8bd7d91ff84ef2e44fbd672057982a3c5a
Grant Olson <kgo@grant-olson.net>
2007-02-10 20:33:46 +0000

    Beginning of screwed up fonts

---
+++ font_test.ml
@@ -0,0 +1,56 @@
+
+let display () =
+  (* Gl.enable `cull_face; *)
+  Gl.enable `texture_2d;
+  (* GlDraw.cull_face `back; *)
+  GlClear.color (0.25, 0.25, 0.25);
+  GlClear.clear [`color];
+  GlClear.clear [`depth];
+  GlDraw.color (1.0, 1.0, 1.0);
+
+  GlDraw.begins `line_strip;
+  GlDraw.vertex2 (0.0,0.0);
+  GlDraw.vertex2 (1.0,0.0);
+  GlDraw.vertex2 (1.0,1.0);
+  GlDraw.vertex2 (0.0,1.0);
+  GlDraw.vertex2 (0.0,0.0);
+  GlDraw.ends ();
+
+  Gl.enable `texture_2d;
+  Texture.set_current_texture "menu/art/font2_prop.tga";
+  (*GlDraw.begins `quads;
+  GlTex.coord2 (0.0,0.0);
+  GlDraw.vertex2 ( (-0.5),(-0.5));
+  GlTex.coord2 (1.0,0.0);
+  GlDraw.vertex2 (0.5,(-0.5));
+  GlTex.coord2 (1.0,1.0);
+  GlDraw.vertex2 (0.5,0.5);
+  GlTex.coord2(0.0,1.0);
+  GlDraw.vertex2 ((-0.5),0.5);
+  GlDraw.ends ();
+  *)
+
+  Q3Fonts.draw_char (-1.0) 1.0 0.0 0.0 'A';
+  Q3Fonts.draw_char 0.0 1.0 1.0 0.0 'Z';
+  Q3Fonts.draw_char (-1.0) 0.0 0.0 (-1.0) 'B';
+  Q3Fonts.draw_char (0.0) 0.0 1.0 (-1.0) 'X';
+  GlMat.mode `projection;
+  GlMat.load_identity ();
+
+
+  Gl.flush ();
+
+  Glut.postRedisplay () ;;
+
+let main () =
+  ignore(Glut.init Sys.argv);
+  Glut.initDisplayMode ~alpha:true ~depth:true () ;
+  Glut.initWindowSize ~w:500 ~h:500 ;
+  ignore(Glut.createWindow ~title:"lablglut & LablGL");
+  Glut.displayFunc ~cb:display;
+
+  Glut.mainLoop();
+  ;;
+
+let _ = main ()
+
---
+++ q3Fonts.ml
@@ -0,0 +1,86 @@
+let prop_map_b  = [|(11, 12, 33);
+                  (49, 12, 31);
+                  (85, 12, 31);
+                  (120, 12, 30);
+                  (156, 12, 21);
+                  (183, 12, 21);
+                  (207, 12, 32);
+
+                  (13, 55, 30);
+                  (49, 55, 13);
+                  (66, 55, 29);
+                  (101, 55, 31);
+                  (135, 55, 21);
+                  (158, 55, 40);
+                  (204, 55, 32);
+
+                  (12, 97, 31);
+                  (48, 97, 31);
+                  (82, 97, 30);
+                  (118, 97, 30);
+                  (153, 97, 30);
+                  (185, 97, 25);
+                  (213, 97, 30);
+
+                  (11, 139, 32);
+                  (42, 139, 51);
+                  (93, 139, 32);
+                  (126, 139, 31);
+                  (158, 139, 25) |]
+
+let propb_gap_width = 4.0
+let propb_space_width = 12.0
+let propb_height = 36.0
+
+type tex_point = {x:float;y:float}
+type tex_box = {top_left:tex_point;bottom_right:tex_point}
+
+let ascii x = int_of_char x
+
+let char_index char =
+  match char with
+      'a' .. 'z' -> (ascii char) - 97
+    | 'A' .. 'Z' -> (ascii char) - 65
+    | _ -> raise Not_found
+
+let char_coords char =
+  let idx = char_index char in
+  let (col,row,width) = prop_map_b.(idx) in
+  let col = float_of_int col in
+  let row = float_of_int row in
+  let width = float_of_int width in
+  let topleft = {x=(col /. 256.0);y=(row /. 256.0)} in
+  let right =(col +. width) /. 256.0 in
+  let bottom = (row +. propb_height) /. 256.0 in
+  let bottomright = {x=right;y=bottom} in
+    {top_left=topleft;bottom_right=bottomright}
+    
+                      
+let tex = Texture.load_texture_from_file "menu/art/font2_prop.tga" 
+
+let draw_char startx starty endx endy char =
+  let tex_coords = char_coords char in
+  let top_left = tex_coords.top_left in
+  let bottom_right = tex_coords.bottom_right in
+  let starts = 1.0 -. top_left.x in
+  let startt = 1.0 -. top_left.y in
+  let ends = 1.0 -. bottom_right.x in
+  let endt = 1.0 -. bottom_right.y in
+    Texture.set_current_texture "menu/art/font2_prop.tga";
+
+
+    GlDraw.begins `quads;
+    
+    GlTex.coord2 (starts,startt);
+    GlDraw.vertex2 ( startx,starty);
+    
+    GlTex.coord2 (ends,startt);
+    GlDraw.vertex2 (endx,starty);
+    
+    GlTex.coord2 (ends,endt);
+    GlDraw.vertex2 (endx,endy);
+    
+    GlTex.coord2(starts,endt);
+    GlDraw.vertex2 (startx,endy);
+
+    GlDraw.ends ()
