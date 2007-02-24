d6d5190e7285998faa78c955ddec991c8143d77c
Grant Olson <kgo@grant-olson.net>
2007-02-24 20:34:47 +0000

    Fixed batch file

--- make_native.bat
+++ make_native.bat
@@ -1,10 +1,15 @@
 del *.cmx*
 
+if NOT "%VCINSTALLDIR%" == "" GOTO COMPILE
+
 call "C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\bin\vcvars32.bat"
 
 
 :: for glut32.lib, where should we really get it?
 set LIB=%LIB%;c:\ruby\bin
 
+:compile
+
 ocamlopt -o c3a.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa binfile.ml tga.ml texture.ml md3.ml player.ml chess_test.ml
 ocamlopt -o player.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa binfile.ml tga.ml texture.ml md3.ml player.ml player_test.ml
+ocamlopt -o fonts.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa binfile.ml tga.ml texture.ml q3Fonts.ml font_test.ml
