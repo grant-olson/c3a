c2d582b9140d42d3bf25a39872d9efac8be03e79
Grant Olson <kgo@grant-olson.net>
2007-06-30 17:09:04 +0000

    updates

--- make_bytecode.bat
+++ make_bytecode.bat
@@ -2,6 +2,6 @@ del *.cmi
 del *.cmo
 del qt.exe
 
-ocamlc -o quaketools.cma -a -I +LablGL lablgl.cma lablglut.cma unix.cma binfile.ml tga.ml texture.ml md3.ml player.ml q3Fonts.ml
+ocamlc -o quaketools.cma -a -I +LablGL lablgl.cma lablglut.cma unix.cma binfile.mli binfile.ml tga.mli tga.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
 ocamlmktop -o qt.exe -I +LablGL lablgl.cma lablglut.cma unix.cma quaketools.cma
 
--- make_native.bat
+++ make_native.bat
@@ -11,9 +11,9 @@ set LIB=%LIB%;c:\ruby\bin
 
 :compile
 
-ocamlopt -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa binfile.ml tga.ml texture.ml md3.ml player.ml q3Fonts.ml
+ocamlopt -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
 ocamlopt -o quaketools.cmxa -a binfile.cmx tga.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
 
-ocamlopt -o c3a.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModels.ml c3a.ml
-ocamlopt -o player.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa player_test.ml
+ocamlopt -o c3a.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModels.ml c3a.ml
+ocamlopt -o player.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa player_test.ml
 ocamlopt -o fonts.exe -I +lablgl lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa font_test.ml
