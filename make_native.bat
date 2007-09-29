e635fff7a5e26307781c90c01260d2061d7322c3
Grant Olson <kgo@grant-olson.net>
2007-09-29 06:32:57 +0000

    updated build

--- make_native.bat
+++ make_native.bat
@@ -8,12 +8,12 @@ call "C:\Program Files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 
 :: for glut32.lib, where should we really get it?
 set LIB=%LIB%;c:\ruby\bin
-
+set DEBUG=-g
 :compile
 
-ocamlopt -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
-ocamlopt -o quaketools.cmxa -a binfile.cmx tga.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
+ocamlopt %DEBUG% -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
+ocamlopt %DEBUG% -o quaketools.cmxa -a binfile.cmx tga.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
 
-ocamlopt -o c3a.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModels.mli c3aModels.ml c3a.ml
-ocamlopt -o player.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa player_test.ml
-ocamlopt -o fonts.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa font_test.ml
+ocamlopt %DEBUG% -o c3a.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModels.mli c3aModels.ml compOpponent.mli compOpponent.ml c3a.ml
+ocamlopt %DEBUG% -o player.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa player_test.ml
+ocamlopt %DEBUG% -o fonts.exe -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa font_test.ml
