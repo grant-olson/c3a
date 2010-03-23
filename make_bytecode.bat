4f5e50752abd22f99f47f16be57151fdbba9da11
Grant Olson <kgo@grant-olson.net>
2010-03-23 18:06:49 +0000

    Fix Linux build

--- make_bytecode.bat
+++
@@ -1,7 +0,0 @@
-del *.cmi
-del *.cmo
-del qt.exe
-
-ocamlc -o quaketools.cma -a -I +lablGL lablgl.cma lablglut.cma unix.cma binfile.mli binfile.ml tga.mli tga.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
-ocamlmktop -o qt.exe -I +lablGL lablgl.cma lablglut.cma unix.cma quaketools.cma
-
--- make_native.sh
+++ make_native.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
 rm -f *.cmx*
-rm -f player.opt c3a.opt fonts.opt
+rm -f player c3a fonts c3a-id-models
 
-#for glut32.lib, where should we really get it?
 DEBUG=
 
+#QUAKE version
 
-ocamlopt $DEBUG -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml texturePath.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
-ocamlopt $DEBUG -o quaketools.cmxa -a binfile.cmx tga.cmx texturePath.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
+cp quake3Models.ml c3aModels.ml
+cp quake3Strings.ml strings.ml
 
-ocamlopt $DEBUG -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml -impl texturePath.alt texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
-ocamlopt $DEBUG -o oatools.cmxa -a binfile.cmx tga.cmx texturePath.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
+ocamlopt $DEBUG -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml strings.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
+ocamlopt $DEBUG -o quaketools.cmxa -a binfile.cmx tga.cmx strings.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
+ocamlopt $DEBUG -o c3a-id-models -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModelsShared.ml c3aModels.mli c3aModels.ml compOpponent.mli compOpponent.ml c3a.ml
 
-ocamlopt $DEBUG -o c3a -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa c3aModelsShared.ml c3aModels.mli c3aModels.ml compOpponent.mli compOpponent.ml c3a.ml
-ocamlopt $DEBUG -o coa -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa oatools.cmxa c3aModelsShared.ml c3aModels.mli -impl c3aModels.alt compOpponent.mli compOpponent.ml c3a.ml
+#OpenArena Models
+
+cp openArenaModels.ml c3aModels.ml
+cp openArenaStrings.ml strings.ml
+
+ocamlopt $DEBUG -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa binfile.mli binfile.ml tga.mli tga.ml strings.ml texture.mli texture.ml md3.mli md3.ml player.mli player.ml q3Fonts.mli q3Fonts.ml
+ocamlopt $DEBUG -o oatools.cmxa -a binfile.cmx tga.cmx strings.cmx texture.cmx md3.cmx player.cmx q3Fonts.cmx
+ocamlopt $DEBUG -o c3a -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa oatools.cmxa c3aModelsShared.ml c3aModels.mli c3aModels.ml compOpponent.mli compOpponent.ml c3a.ml
+
+# OTHER STUFF
+
 ocamlopt $DEBUG -o player.opt -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa player_test.ml
 ocamlopt $DEBUG -o fonts.opt -I +lablGL lablgl.cmxa lablglut.cmxa unix.cmxa quaketools.cmxa font_test.ml
 
