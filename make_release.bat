c01f67dd4b75db86d52e489e223c16e6db524cb7
Grant Olson <kgo@grant-olson.net>
2010-03-25 14:01:53 +0000

    Updates to release build.

--- make_release.bat
+++ make_release.bat
@@ -1,8 +1,10 @@
 set DATESTAMP=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
 set SRCRELEASE=c3a-source-%DATESTAMP%.tar
 set EXERELEASE=c3a-exe-%DATESTAMP%.zip
+set OASRCRELEASE=openarena-model-blender-source-files-%DATESTAMP%.tar
 
 mkdir distprep
+mkdir releases
 
 :: Get the code
 svn export svn+ssh://grant@johnwhorfin/var/local/svn/c3a/trunk distprep/c3a
@@ -10,12 +12,20 @@ svn export svn+ssh://grant@johnwhorfin/var/local/svn/c3a/trunk distprep/c3a
 :: Remove Quake 3 models, can't legally redistribute.
 rmdir /S /Q distprep\c3a\pak0
 
-:: Make source distribution
+:: Source is a seperate distribution
+
+move distprep\c3a\openarena_src distprep\
+
+:: Make source distributions
 
 cd distprep
 tar cvf %SRCRELEASE% c3a
 gzip %SRCRELEASE%
 set SRCRELEASE=%SRCRELEASE%.gz
+
+tar cvf %OASRCRELEASE% openarena_src
+gzip %OASRCRELEASE%
+set OASRCRELEASE=%OASRCRELEASE%.gz
 
 :: Make binary distribution
 cd c3a
@@ -45,10 +55,12 @@ mkdir releases
 
 copy distprep\%EXERELEASE% releases
 copy distprep\%SRCRELEASE% releases
+copy distprep\%OASRCRELEASE% releases
 
 cd releases
 gpg --armor --detach-sign %EXERELEASE%
 gpg --armor --detach-sign %SRCRELEASE%
+gpg --armor --detach-sign %OASRCRELEASE%
 
 cd ..
 rmdir /S /Q distprep
